4fcacb05b3abeb0b0cd2506dce2dafc1a321d744
BenWoschi <benjaminw4101@gmail.com>
2025-09-10 20:36:14 -0400

    Background for Instructions Challenge Finished
    Added background, including foreground elements

--- topics/instructions/instructions-challenge/js/script.js
+++ topics/instructions/instructions-challenge/js/script.js
@@ -1,24 +1,92 @@
 /**
- * Title of Project
- * Author Name
+ * Instructions Challenge
+ * Ben Woschitz
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Drawing a landscape on a canvas
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Sets up the canvas
 */
 function setup() {
-
+    createCanvas(1920, 1080);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Background
 */
 function draw() {
+    // The Sky and background
+    background("#604d6c");
 
+    //The Sand
+    drawSand();
+
+    //Sky Gradient
+    drawGradient();
+
+    //Background Sand
+    drawLightsand();
+
+    //Distant Sand
+    drawDistantsand();
+
+    //Sun Red
+    drawSunred();
+
+    //Sun Light
+    drawSunlight();
+}
+
+function drawSand() {
+    //Rectangle
+    push();
+    noStroke();
+    fill("#473c36");
+    rect(0, 525, 1920, 575);
+    pop();
+}
+
+function drawGradient() {
+    push();
+    noStroke();
+    fill(117, 75, 98, 50);
+    rect(0, 0, 1920, 200);
+    pop();
+}
+
+function drawLightsand() {
+    push();
+    noStroke();
+    fill(67, 60, 56, 120);
+    rect(0, 650, 1920, 550);
+    pop();
+}
+
+function drawDistantsand() {
+    push();
+    noStroke();
+    fill(59, 60, 56, 175);
+    rect(0, 515, 1920, 10);
+    pop();
+}
+
+function drawSunred() {
+    push();
+    noStroke();
+    fill("#fd3c2c")
+    ellipse(1500, 450, 75, 75);
+    pop();
+}
+
+function drawSunlight() {
+    push();
+    stroke(163,78,99,100);
+    strokeWeight(5);
+    fill("#fad5dc");
+    ellipse(1350, 275, 75, 75);
+    pop();
 }
