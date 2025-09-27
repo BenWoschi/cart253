3186acc915b4d65e1e1d1a18f657bcaabd036d4a
BenWoschi <benjaminw4101@gmail.com>
2025-09-27 19:43:38 -0400

    Started Art Jam Project
    Began setup of the art jam file and canvas

---
+++ topics/art-jam/bw-artjam/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Art Jam
+ * Ben Woschitz
+ * 
+ * Abstract representation of myself
+ */
+
+"use strict";
+
+/**
+ * Creates Canvas
+*/
+function setup() {
+    createCanvas(400, 400);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
