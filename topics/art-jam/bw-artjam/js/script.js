cce475070b69ff5d8f9426ec08ad271701187802
BenWoschi <benjaminw4101@gmail.com>
2025-09-28 18:10:39 -0400

    Created flower in center
    Flower shape has been created in the center of the canvas

--- topics/art-jam/bw-artjam/js/script.js
+++ topics/art-jam/bw-artjam/js/script.js
@@ -6,18 +6,56 @@
  */
 
 "use strict";
+const flowerMiddle = {
+    size: 100,
+    fill: "#090309ff"
+    };
+
+let petals = {
+    width: 60,
+    height: 150,
+    fill: "#120613"
+    };
+    
+let petalsAmount = 9;
+let petalsAngle = TWO_PI / petalsAmount;
 
 /**
  * Creates Canvas
 */
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the flower along with cursor "light"
 */
 function draw() {
+    background("#120d1c");
 
+//Flower
+    drawFlowerMiddle();
+    drawPetals();
+}
+
+function drawFlowerMiddle() {
+    push();
+    noStroke();
+    fill(flowerMiddle.fill);
+    ellipse(width / 2, height / 2, flowerMiddle.size);
+    pop();
+}
+
+function drawPetals() {
+    push();
+    translate(width / 2, height / 2);
+    noStroke();
+    fill(petals.fill);
+    //Petals rotate around center
+    for (let i = 0; i < petalsAmount; i++) {
+        rotate(TWO_PI / petalsAmount);
+        ellipse(0, flowerMiddle.size / 3 + petals.height / 4, petals.width, petals.height);
+    }
+    pop();
 }
