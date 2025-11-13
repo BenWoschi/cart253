8e1a6207c173f1df5a7bb7d865c0cf6caa030b61
BenWoschi <benjaminw4101@gmail.com>
2025-11-12 20:24:45 -0500

    Started and finished Data Challenge
    Completed the challenge on my desktop so I'm pushing the final version here.

---
+++ topics/data/data-challenge/js/script.js
@@ -0,0 +1,62 @@
+/**
+ * Terrible New Car
+ * Pippin Barr
+ * 
+ * A program to generate new car model names using dinosaurs.
+ * 
+ * Uses:
+ * Darius Kazemi's corpora repository
+ * https://github.com/dariusk/corpora/tree/master
+ */
+
+"use strict";
+
+let carData = undefined;
+let dinoData = undefined;
+let langData = undefined;
+let lang = "fr";
+
+// Starts with the instruction
+let carName = "Click to generate a car name.";
+
+/**
+ * Load the car and dinosaur data
+ */
+function preload() {
+    carData = loadJSON('assets/data/cars.json');
+    dinoData = loadJSON('assets/data/dinosaurs.json');
+}
+
+/**
+ * Create the canvas
+*/
+function setup() {
+    createCanvas(600, 400);
+}
+
+/**
+ * Display the current main text (either instructions or a car)
+*/
+function draw() {
+    background(0);
+
+    push();
+    fill("pink");
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(carName, width / 2, height / 2);
+    pop();
+}
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {
+    generateName();
+}
+
+function generateName() {
+    let randomCar = random(carData.cars);
+    let randomDino = random(dinoData.dinosaurs);
+    carName = randomCar + " " + randomDino; 
+}
